73d4912f4f88a8ceb9a40125f7df6c8fee761d5a
Luke Panayi <lp721@beech12.doc.ic.ac.uk>
2024-10-23 14:39:09 +0100

    updated example script

--- vary_rob_lsq.py
+++ vary_rob_lsq.py
@@ -3,18 +3,18 @@ import psutil
 import time
 
 sizes = [2**i for i in range(4,10)] #16..512
-mininum_int_phys_reg_size = 49 #as informed by simulate.py
+mininum_phys_reg_size = 49 #as informed by simulate.py
 
 for i, rob_size in enumerate(sizes):
     for lsq_size in sizes:
         name = f"rob_{rob_size}_lsq_{lsq_size}"
 
-        if rob_size < mininum_int_phys_reg_size: 
-            int_phys_regs = mininum_int_phys_reg_size
+        if rob_size < mininum_phys_reg_size: 
+            phys_regs = mininum_phys_reg_size
         else: 
-            int_phys_regs = rob_size
+            phys_regs = rob_size
 
         #load balancer if running jobs in parallel. runs will crash if cpu usage gets too high.
         #while psutil.cpu_percent() > 70: time.sleep(30)
         #.run won't return until the program finishes. using .Popen returns immediately, meaning all runs execute in parallel (be mindful of your machine's resources though!). however, it has fewer features like capturing program output.
-        subprocess.run("python /homes/lp721/aca-gem5/simulate.py --rob-size "+str(rob_size)+" --num-int-phys-regs "+str(int_phys_regs)+" --num-float-phys-regs "+str(rob_size)+" --num-vec-phys-regs "+str(rob_size)+" --lsq-size "+str(lsq_size)+" --name "+name, shell=True)
+        subprocess.run("python /homes/lp721/aca-gem5/simulate.py --rob-size "+str(rob_size)+" --num-int-phys-regs "+str(phys_regs)+" --num-float-phys-regs "+str(phys_regs)+" --num-vec-phys-regs "+str(phys_regs)+" --lsq-size "+str(lsq_size)+" --name "+name, shell=True)
